710126e588c548c64edaeea676574d7e64ddcf1a
Kamogelo44 <kamogeloobakeng44@gmail.com>
2026-04-09 15:19:52 +0200

    feat: Update cart icon and count display across multiple pages for consistency feat: Setup the database config files and tables in mysql. -Add database schema - users, products, orders, transactions, reviews, categories.
    -Add database configuration file.

--- php/config.php
+++ php/config.php
@@ -3,7 +3,18 @@
  * ConsuTrade - Database Configuration
  * Author: Kamogelo Phale
  *
- * This file handles the MySQL database connection.
- * Database credentials will be set up during hosting configuration in Week 5.
  */
+
+$host = 'localhost';
+$db_name = 'consutrade';
+$username = 'root';
+$password = '';
+
+$conn = new mysqli($host, $username, $password, $db_name);
+
+if ($conn->connect_error) {
+    die('Database connection failed' .$conn->connect_error);
+}
+
+$conn->set_charset('utf8mb4');
 ?>
